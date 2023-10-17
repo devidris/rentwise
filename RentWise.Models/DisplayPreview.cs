9b1accb625eeb1e5caeaaae763f091a8366c09b9
Idris <idris@exarotech.com>
2023-10-17 14:25:40 +0100

    feat: UI/UX for construction section done

---
+++ RentWise.Models/DisplayPreview.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentWise.Models
+{
+    public class DisplayPreview
+    {
+        public string Image { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public double Rating { get; set; }
+        public string Location { get; set; }
+
+    }
+}
