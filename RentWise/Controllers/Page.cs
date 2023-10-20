0216eb65942cf86aae1849e42fd530facddf236d
Idris <idris@exarotech.com>
2023-10-20 06:41:14 +0100

    feat:About Us UI/UX

---
+++ RentWise/Controllers/Page.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RentWise.Controllers
+{
+    public class Page : Controller
+    {
+        public IActionResult About()
+        {
+            return View();
+        }
+    }
+}
