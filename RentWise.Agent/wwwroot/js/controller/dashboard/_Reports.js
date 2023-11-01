6ac9f8a7053853868cddfda576054a609dcc380a
Idris <idris@exarotech.com>
2023-11-01 17:26:05 +0100

    feat:Orders and Review UI/UX

---
+++ RentWise.Agent/wwwroot/js/controller/dashboard/_Reports.js
@@ -0,0 +1,50 @@
+﻿function onToggleReport(contentNo) {
+    document.querySelectorAll(".report").forEach((content) => {
+        content.classList.add("display-none");
+
+    })
+    document.querySelectorAll(".report-tab").forEach((content) => {
+        content.classList.remove("report-active");
+
+    })
+
+    document.querySelector(".report-" + contentNo).classList.remove("display-none");
+    document.querySelector(".report-tab-" + contentNo).classList.add("report-active");
+}
+
+onToggleReport(1);
+document.addEventListener('DOMContentLoaded', function () {
+    var ctx = document.getElementById('overview-report').getContext('2d');
+
+    var data = {
+        labels: ['Number of Sales', 'Total number of orders'],
+        datasets: [{
+            label: 'Count',
+            data: [10, 20], // Replace with your actual data
+            backgroundColor: [
+                'rgba(0, 128, 128, 1)',
+                'rgba(75, 0, 130, 1)'
+            ],
+            borderColor: [
+                'rgba(0, 128, 128, 1)',
+                'rgba(75, 0, 130, 1)'
+            ],
+            borderWidth: 1,
+            barThickness: 20 // Set the width of each bar
+        }]
+    };
+
+    var options = {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    };
+
+    var myChart = new Chart(ctx, {
+        type: 'bar',
+        data: data,
+        options: options
+    });
+});
