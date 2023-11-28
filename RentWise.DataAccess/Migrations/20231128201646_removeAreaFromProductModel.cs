9a2b2134b29498efb1713821b9bbd642b66a5ecd
Idris <idris@exarotech.com>
2023-11-28 21:42:04 +0100

    Fix sadat corrections

---
+++ RentWise.DataAccess/Migrations/20231128201646_removeAreaFromProductModel.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RentWise.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class removeAreaFromProductModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
