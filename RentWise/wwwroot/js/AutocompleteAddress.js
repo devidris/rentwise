714b0ce2b36f8149218678f32245d3d56e2bf6b7
Idris <idris@exarotech.com>
2023-11-29 11:23:28 +0100

    Cleaned up index page

---
+++ RentWise/wwwroot/js/AutocompleteAddress.js
@@ -0,0 +1,17 @@
+﻿
+// Initialize the Places Autocomplete
+const autocomplete = new google.maps.places.Autocomplete(document.getElementById('autocomplete-input'));
+
+// Set the types to restrict the search to addresses only
+autocomplete.setTypes(['address']);
+
+// Listen for the event when a place is selected
+autocomplete.addListener('place_changed', function () {
+    const place = autocomplete.getPlace();
+    if (place.geometry && place.geometry.location) {
+        const latitude = place.geometry.location.lat();
+        const longitude = place.geometry.location.lng();
+        lat = latitude
+        lng = longitude
+    }
+});
--- RentWise/wwwroot/js/site.js
+++ RentWise/wwwroot/js/site.js
@@ -1,4 +1,34 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿function buildQueryParams(key, value, currentUrl = new URL(location.href), deleteIfNotExist = false) {
+    if (key && value) {
+        // Check if the 'key' parameter already exists
+        if (currentUrl.searchParams.has(key)) {
+            // If it exists, update its value
+            currentUrl.searchParams.set(key, value);
+        } else {
+            // If it doesn't exist, add the 'value' parameter
+            currentUrl.searchParams.append(key, value);
+        }
+    }
+    if (deleteIfNotExist) {
+        if (!key && !value) {
+            // If it does not exists, delete its value
+            currentUrl.searchParams.delete(key, value);
+        }
+    }
+    return currentUrl;
+}
 
-// Write your JavaScript code.
+function openModel(name, isId = true) {
+    if (isId) {
+        $('#' + name).modal('show')
+    } else {
+        $('.' + name).modal('show')
+    }
+}
+function closeModel(name, isId = true) {
+    if (isId) {
+        $('#' + name).modal('hide')
+    } else {
+        $('.' + name).modal('hide')
+    }
+}
