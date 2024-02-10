dc9779d362c535eb28db36f04f76082d3531ff9f
Idris <idris@exarotech.com>
2024-02-10 09:18:50 +0100

    Firebase push notification

---
+++ RentWise/wwwroot/firebase-messaging-sw.js
@@ -0,0 +1,15 @@
+﻿importScripts('https://www.gstatic.com/firebasejs/9.14.0/firebase-app-compat.js')
+importScripts('https://www.gstatic.com/firebasejs/9.14.0/firebase-messaging-compat.js')
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBaSMKCA0Toe3O_4LFHUxgEEDhf_-avHwY",
+    authDomain: "rent-wise-dc09f.firebaseapp.com",
+    projectId: "rent-wise-dc09f",
+    storageBucket: "rent-wise-dc09f.appspot.com",
+    messagingSenderId: "342704319982",
+    appId: "1:342704319982:web:c238396bf5ebac8aa6b2ff",
+    measurementId: "G-QYWYSJ5BTE"
+};
+const app = firebase.initializeApp(firebaseConfig)
+const messaging = firebase.messaging()
